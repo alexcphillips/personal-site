9148d13df205703b5b3142ee40a1ec3ebdbf5b22
Alexander Phillips <alexphillipsdev@gmail.com>
2022-01-09 13:39:52 -0600

    moves env config into server/index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const mongo = require("./mongo");
 const { app } = require("./app");
+const path = require("path");
+
+envPath =
+  process.env.NODE_ENV === "dev" ? "../.env.dev" : (envPath = "../.env");
+require("dotenv").config({ path: path.join(__dirname, envPath) });
 
 (async () => {
-  await mongo.mongoConnect();
-
-  console.log("mongo connection SUCCESSFUL");
+  await mongo.mongoConnect(process.env.MONGO_URI);
 
   app.listen(process.env.PORT, () => {
     console.log("Server started SUCCESSFULLY on port", process.env.PORT);
