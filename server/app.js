aa647ca48a66ec2128d1b26ae14258c7f639b0a2
Alexander Phillips <alexphillipsdev@gmail.com>
2022-01-09 16:26:56 -0600

    changed app.js structure

--- server/app.js
+++ server/app.js
@@ -1,17 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const routes = require("./routes");
-const staticImages = require("path").join(__dirname, "../build");
-
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(staticImages));
-app.use((req, res) => {
-  console.log(req.url, "HELLO");
-});
-
+app.use(express.static(path.join(__dirname, "../build")));
 app.use(routes);
 
 module.exports = { app };
