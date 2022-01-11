4c1dd93dc09e3bac7bf51d3dc6e384961beb5291
Alexander Phillips <alexphillipsdev@gmail.com>
2022-01-11 11:38:58 -0600

    fix db seeding bug

--- server/mongo/operations.js
+++ server/mongo/operations.js
@@ -14,10 +14,20 @@ exports.findOne = async (collection, key, val, project = {}) => {
   return await mongo.db.collection(collection).findOne(opts, project);
 };
 
-exports.findMany = async (collection, key, val, project = {}) => {
-  const opts = {};
-  opts[key] = val;
-  return await mongo.db.collection(collection).find(opts, project);
+exports.findMany = async (collection, query, projection = {}) => {
+  const opts = {
+    projection
+  };
+
+  console.log(query);
+  const result = await mongo.db
+    .collection(collection)
+    .find(query, opts)
+    .toArray();
+  // const result = await mongo.db.collection("images").find({}).toArray();
+
+  console.log("result:", result);
+  return result;
 };
 
 exports.insertOne = async (collection, doc) => {
--- server/scripts/dbSeed.js
+++ server/scripts/dbSeed.js
@@ -2,18 +2,23 @@ const { findMany, insertMany } = require("../mongo/operations");
 const docs = require("./familyImages.json");
 
 exports.seedFamilyImages = async () => {
-  const dbDocs = await (
-    await findMany("images", { page: "family" }, "name")
-  ).toArray();
+  const dbDocs = await findMany(
+    "images",
+    { page: "family" },
+    { name: 1, _id: 0 }
+  );
 
   const dbDocNames = dbDocs.map((doc) => doc.name);
   const missingDocs = [];
+
+  console.log("docs:", dbDocs);
 
   for (const doc of docs) {
     if (!dbDocNames.includes(doc.name)) {
       missingDocs.push(doc);
     }
   }
+  console.log(missingDocs);
 
   if (missingDocs.length) await insertMany("images", missingDocs);
 };
