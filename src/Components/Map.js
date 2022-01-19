3e0d4d6044de24d079ea0b766aea7eac4ff1d7dd
Alexander Phillips <alexphillipsdev@gmail.com>
2022-01-19 17:40:27 -0600

    adds initial map grid for game

---
+++ src/Components/Map.js
@@ -0,0 +1,8 @@
+const { map } = require("./data/mapGrid");
+
+for (let x = 0; x < map.length; x++) {
+  console.log("x position:", x);
+  for (let y = 0; y < map[x].length; y++) {
+    console.log(map[x][y]);
+  }
+}
---
+++ src/Components/data/mapGrid.js
@@ -0,0 +1,58 @@
+const forest = {
+  biome: "forest"
+};
+
+const plains = {
+  biome: "plains"
+};
+
+const lava = {
+  biome: "volcano",
+  damage: 1
+};
+
+const wallMountain = {
+  isWall: true,
+  wallType: "mountain"
+};
+
+const coastline = {
+  isWall: true,
+  wallType: "coastline"
+};
+
+const forestIsland = {
+  isWall: true,
+  wallType: "forestIsland"
+};
+
+const ocean = {
+  isWall: true,
+  wallType: "ocean"
+};
+
+const forestDungeon = {
+  biome: "forest",
+  hasDungeon: true
+};
+
+const mountainCave = {
+  biome: "mountain",
+  hasDungeon: true
+};
+
+exports.map = [
+  [
+    wallMountain,
+    wallMountain,
+    forestDungeon,
+    coastline,
+    forestIsland,
+    coastline
+  ],
+  [wallMountain, lava, plains, forest, plains, coastline],
+  [lava, lava, plains, forest, forest, coastline],
+  [lava, plains, wallMountain, wallMountain, coastline, ocean],
+  [plains, plains, mountainCave, wallMountain, coastline, ocean],
+  [plains, wallMountain, wallMountain, wallMountain, ocean, ocean]
+];
