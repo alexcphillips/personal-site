d1e630ce96f25408bd69ee715efd6090db5e331c
Alexander Phillips <alexphillipsdev@gmail.com>
2022-01-26 09:22:09 -0600

    fix css padding bottom on mobile, and hides download link for mobile

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,37 +1,41 @@
 import "./Home.css";
+import Container from "react-bootstrap/Container";
 
 export default function Home() {
   return (
-    <div id="page">
-      <div id="main-container">
-        <h3 id="greeting">Hi, I'm Alex</h3>
-        <h6 id="heading">I'm a Node.js Developer.</h6>
-        <p className="body-text">
-          I write code in Node.js professionally and started over 2 years ago,
-          and I began coding with my father at a young age. I enjoy solving new
-          challenges and growing as a developer. I put in the time to my craft
-          to overcome obstacles and exceed all expectations.
-        </p>
-        <h6 id="heading">My Favorite Projects</h6>
-        <p className="body-text">
-          My current project is an Excel file processing application I built in
-          microservices. The microservices included Express REST APIs and data
-          processing systems.
+    <Container>
+      <div id="page">
+        <div id="main-container">
+          <h3 id="greeting">Hi, I'm Alex</h3>
+          <h6 id="heading">I'm a Node.js Developer.</h6>
+          <p className="body-text">
+            I write code in Node.js professionally and started over 2 years ago,
+            and I began coding with my father at a young age. I enjoy solving
+            new challenges and growing as a developer. I put in the time to my
+            craft to overcome obstacles and exceed all expectations.
+          </p>
+          <h6 id="heading">My Favorite Projects</h6>
+          <p className="body-text">
+            My current project is an Excel file processing application I built
+            in microservices. The microservices included Express REST APIs and
+            data processing systems.
+            <br />
+            Users stream Excel files through the React frontend to the file
+            processing API. The API stores the Excel file in an AWS S3 Bucket
+            using AWS-SDK. The data processing service converts the Excel file
+            data into JSON for storage and retrieval with MongoDB. The services
+            cache and communicate using Redis.
+          </p>
           <br />
-          Users stream Excel files through the React frontend to the file
-          processing API. The API stores the Excel file in an AWS S3 Bucket
-          using AWS-SDK. The data processing service converts the Excel file
-          data into JSON for storage and retrieval with MongoDB. The services
-          cache and communicate using Redis.
-        </p>
-        <br />
-        <p className="body-text">
-          I built a RESTful API with Express for a financial services
-          application. My API stored profit loss statements, invoices, and
-          expense data into MongoDB. I included routes to validate, store,
-          retrieve, and update documents.
-        </p>
+          <p className="body-text">
+            I built a RESTful API with Express for a financial services
+            application. My API stored profit loss statements, invoices, and
+            expense data into MongoDB. I included routes to validate, store,
+            retrieve, and update documents.
+          </p>
+        </div>
+        <div id="footer" />
       </div>
-    </div>
+    </Container>
   );
 }
