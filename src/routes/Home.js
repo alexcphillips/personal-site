b431f94b87bb9313aa60d027d49ac482fcfef84f
Alexander Phillips <alexphillipsdev@gmail.com>
2022-01-26 13:21:32 -0600

    changes common.css spacer names and reformats home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -8,30 +8,32 @@ export default function Home() {
     <Container>
       <div className="page">
         <div className="main-container">
-          <div className="spacer-medium" />
+          <div className="medium-spacer" />
           <h3 id="greeting">Hi, I'm Alex</h3>
-          <div className="spacer-small" />
+          <div className="tiny-spacer" />
           <h6 className="heading text-center">I'm a Node.js Developer.</h6>
-          <div className="spacer-medium" />I write code in Node.js
-          professionally and started over 2 years ago, and I began coding with
-          my father at a young age. I enjoy solving new challenges and growing
-          as a developer. I put in the time to my craft to overcome obstacles
-          and exceed all expectations.
+          <div className="medium-spacer" />
+          I write code in Node.js professionally and started over 2 years ago,
+          and I began coding with my father at a young age. I enjoy solving new
+          challenges and growing as a developer. I put in the time to my craft
+          to overcome obstacles and exceed all expectations.
+          <div className="medium-spacer" />
           <h6 className="heading">My Favorite Projects</h6>
-          <div className="heading">Excel processing application</div>
+          <div className="small-spacer" />
+          <div className="heading">• Excel processing application</div>
+          <div className="miniscule-spacer" />
           My current project is an Excel file processing application I built in
           microservices. The microservices included Express REST APIs and data
           processing systems.
-          <br />
-          <br />
+          <div className="small-spacer" />
           Users stream Excel files through the React frontend to the file
           processing API. The API stores the Excel file in an AWS S3 Bucket
           using AWS-SDK. The data processing service converts the Excel file
           data into JSON for storage and retrieval with MongoDB. The services
           cache and communicate using Redis.
-          <br />
-          <br />
-          <div className="heading">Rest API for financial services</div>
+          <div className="small-spacer" />
+          <div className="heading">• Rest API for financial services</div>
+          <div className="miniscule-spacer" />
           <p className="body-text">
             I built a RESTful API with Express for a financial services
             application. My API stored profit loss statements, invoices, and
