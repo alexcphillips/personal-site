821121c29f078b22176549cad856377177383745
Alexander Phillips <alexphillipsdev@gmail.com>
2022-02-02 10:30:16 -0600

    fix wording and add install docker compose

--- src/Components/notes/index.js
+++ src/Components/notes/index.js
@@ -1,11 +1,13 @@
 import Ec2 from "./ec2";
 import sshEc2 from "./sshEc2";
 import installDockerOnEc2 from "./installDockerOnEc2";
+import installDockerComposeOnEc2 from "./installDockerComposeOnEc2";
 
 let notes = {
-  "launching-an-aws-ec2-instance": Ec2,
-  "ssh-into-an-aws-ec2-instance": sshEc2,
-  "install-docker-on-ec2-instance": installDockerOnEc2
+  "launching-an-aws-ec2": Ec2,
+  "ssh-into-an-aws-ec2": sshEc2,
+  "install-docker-on-ec2": installDockerOnEc2,
+  "install-docker-compose-on-ec2": installDockerComposeOnEc2
 };
 
 export default notes;
