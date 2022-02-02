d015f9f78bcfb52dd6e1f0c084eca68c7360171e
Alexander Phillips <alexphillipsdev@gmail.com>
2022-02-02 10:47:31 -0600

    remove bootstrap nav

--- src/Components/LeftNav.js
+++ src/Components/LeftNav.js
@@ -1,25 +1,27 @@
 import "../common.css";
 import "./Side.css";
 
-import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import noteLinks from "./noteLinks";
 
 export default function LeftNav() {
   return (
-    <>
-      <Nav className="tall bg-light left">
-        <div className="small-spacer" />
+    <div className="tall bg-light left">
+      <div className="small-spacer" />
 
-        {noteLinks.map(({ to, text }, index) => {
-          return (
-            <Link key={`left-nav-${index}`} to={to} className="link-text">
-              {text}
-            </Link>
-          );
-        })}
-      </Nav>
-    </>
+      {noteLinks.map(({ to, text }, index) => {
+        return (
+          <Link
+            key={`left-nav-${index}`}
+            to={to}
+            className="link-text"
+            style={{ display: "block" }}
+          >
+            {text}
+          </Link>
+        );
+      })}
+    </div>
   );
 }
