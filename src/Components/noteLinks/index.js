027840188520d851a763d856730e28e615701cbe
Alexander Phillips <alexphillipsdev@gmail.com>
2022-02-02 10:22:44 -0600

    adds pages to links

--- src/Components/noteLinks/index.js
+++ src/Components/noteLinks/index.js
@@ -1,6 +1,8 @@
 import ec2 from "./ec2";
 import sshEc2 from "./sshEc2";
+import installDockerOnEc2 from "./installDockerOnEc2";
+import installDockerComposeOnEc2 from "../notes/installDockerComposeOnEc2";
 
-const noteLinks = [ec2, sshEc2];
+const noteLinks = [ec2, sshEc2, installDockerOnEc2, installDockerComposeOnEc2];
 
 export default noteLinks;
