e2fce366986d2c7887bed38839ecf02c47581070
Alexander Phillips <alexphillipsdev@gmail.com>
2022-10-06 19:37:30 -0500

    fix error in resume download

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,49 +4,58 @@ import "./Home.css";
 import Container from "react-bootstrap/Container";
 
 export default function Home() {
-  return (
-    <Container>
-      <div className="page">
-        <h3 id="greeting">
-          Hi, I'm Alex
-          <br />
-        </h3>
-        <h6 className="heading text-center">I'm a Fullstack Developer.</h6>
+    return (
+        <Container>
+            <div className="page">
+                <h3 id="greeting">
+                    Hi, I'm Alex
+                    <br />
+                </h3>
+                <h6 className="heading text-center">
+                    I'm a Node.js Developer.
+                </h6>
 
-        <div className="main-container">
-          <div className="medium-spacer" />
-          <div className="tiny-spacer" />
-          <div className="medium-spacer" />
-          I write code in Node.js and React professionally and started over 2
-          years ago, and I began coding with my father at a young age. I enjoy
-          solving new challenges and growing as a developer. I put in the time
-          to my craft to overcome obstacles and exceed all expectations.
-          <div className="medium-spacer" />
-          <h6 className="heading">My Favorite Projects</h6>
-          <div className="small-spacer" />
-          <div className="heading">• Excel processing application</div>
-          <div className="miniscule-spacer" />
-          My current project is an Excel file processing application I built in
-          microservices. The microservices included Express REST APIs and data
-          processing systems.
-          <div className="small-spacer" />
-          Users stream Excel files through the React frontend to the file
-          processing API. The API stores the Excel file in an AWS S3 Bucket
-          using AWS-SDK. The data processing service converts the Excel file
-          data into JSON for storage and retrieval with MongoDB. The services
-          cache and communicate using Redis.
-          <div className="small-spacer" />
-          <div className="heading">• Rest API for financial services</div>
-          <div className="miniscule-spacer" />
-          <p className="body-text">
-            I built a RESTful API with Express for a financial services
-            application. My API stored profit loss statements, invoices, and
-            expense data into MongoDB. I included routes to validate, store,
-            retrieve, and update documents.
-          </p>
-        </div>
-        <div id="footer" />
-      </div>
-    </Container>
-  );
+                <div className="main-container">
+                    <div className="medium-spacer" />
+                    <div className="tiny-spacer" />
+                    <div className="medium-spacer" />
+                    I write code in Node.js professionally and started over 4
+                    years ago, when I began coding with my father. I enjoy
+                    solving new challenges and growing as a developer. I put in
+                    the time to my craft to overcome obstacles and exceed all
+                    expectations.
+                    <div className="medium-spacer" />
+                    <h6 className="heading">My Favorite Projects</h6>
+                    <div className="small-spacer" />
+                    <div className="heading">
+                        • Excel processing application
+                    </div>
+                    <div className="miniscule-spacer" />
+                    My current project is an Excel file processing application I
+                    built in microservices. The microservices included Express
+                    REST APIs and data processing systems.
+                    <div className="small-spacer" />
+                    Users stream Excel files through the React frontend to the
+                    file processing API. The API stores the Excel file in an AWS
+                    S3 Bucket using AWS-SDK. The data processing service
+                    converts the Excel file data into JSON for storage and
+                    retrieval with MongoDB. The services cache and communicate
+                    using Redis.
+                    <div className="small-spacer" />
+                    <div className="heading">
+                        • Rest API for financial services
+                    </div>
+                    <div className="miniscule-spacer" />
+                    <p className="body-text">
+                        I built a RESTful API with Express for a financial
+                        services application. My API stored profit loss
+                        statements, invoices, and expense data into MongoDB. I
+                        included routes to validate, store, retrieve, and update
+                        documents.
+                    </p>
+                </div>
+                <div id="footer" />
+            </div>
+        </Container>
+    );
 }
--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -4,96 +4,113 @@ import "./Resume.css";
 import { saveAs } from "file-saver";
 
 const saveResume = () =>
-  saveAs("http://localhost:3001/downloads/resume.docx", "Alex Phillips.docx");
+    saveAs("http://localhost:3001/downloads/resume.docx", "Alex Phillips.docx");
 
 export default function Resume() {
-  return (
-    <div className="page">
-      <h1 id="name">Alexander Phillips</h1>
-      <div className="main-container">
-        <div className="inner-container">
-          <div className="spacer-small" />
-          <h4 className="heading">Node.js Developer</h4>
-          {/* <div style={{ marginBottom: "1rem" }} /> */}I started work as a
-          Node.js Developer at ANI Consulting on August of 2019. I build
-          microservices, REST APIs, and frontends.
-          <div className="section">
-            <div className="section-head left">SKILLS</div>
-            <ul className="left">
-              <li>Building Node.js applications</li>
-              <li>ES6</li>
-              <li>React and JSX</li>
-              <li>Utilizing MongoDB</li>
-              <li>Writing Jest tests</li>
-              <li>Debugging</li>
-            </ul>
-          </div>
-          <div className="section">
-            <div className="section-head left">TOOLS</div>
-            <ul className="left">
-              <li>Git/GitHub</li>
-              <li>Jira</li>
-              <li>Postman</li>
-              <li>Visual Studio Code</li>
-            </ul>
-          </div>
-          <div className="section">
-            <div className="section-head left">SITES</div>
-            <a
-              className="left"
-              target="_blank"
-              href="http://github.com/alexcphillips"
-            >
-              http://github.com/alexcphillips
-            </a>
-            <br />
-            <a
-              className="left"
-              target="_blank"
-              href="http://codewars.com/users/alexphillips"
-            >
-              http://codewars.com/users/alexphillips
-            </a>
-            <br />
+    return (
+        <div className="page">
+            <h1 id="name">Alexander Phillips</h1>
+            <div className="main-container">
+                <div className="inner-container">
+                    <div className="spacer-small" />
+                    <h4 className="heading">Node.js Developer</h4>
+                    {/* <div style={{ marginBottom: "1rem" }} /> */}I started
+                    work as a Node.js Developer at ANI Consulting in 2019. I
+                    build microservices, REST APIs, and love learning new
+                    things.
+                    <div className="section">
+                        <div className="section-head left">SKILLS</div>
+                        <ul className="left">
+                            <li>Building Node.js applications</li>
+                            <li>ES6</li>
+                            <li>React and JSX</li>
+                            <li>Utilizing MongoDB</li>
+                            <li>Writing Jest tests</li>
+                            <li>Debugging</li>
+                        </ul>
+                    </div>
+                    <div className="section">
+                        <div className="section-head left">TOOLS</div>
+                        <ul className="left">
+                            <li>Git/GitHub</li>
+                            <li>Jira</li>
+                            <li>Postman</li>
+                            <li>Visual Studio Code</li>
+                        </ul>
+                    </div>
+                    <div className="section">
+                        <div className="section-head left">SITES</div>
+                        <a
+                            className="left"
+                            target="_blank"
+                            href="http://github.com/alexcphillips"
+                        >
+                            http://github.com/alexcphillips
+                        </a>
+                        <br />
+                        <a
+                            className="left"
+                            target="_blank"
+                            href="http://codewars.com/users/alexphillips"
+                        >
+                            http://codewars.com/users/alexphillips
+                        </a>
+                        <br />
 
-            <a className="left" target="_blank" href="http://alexcphillips.com">
-              http://alexcphillips.com
-            </a>
-            <div style={{ marginBottom: "1rem" }} />
-          </div>
-          <div className="section">
-            <div className="section-head left">WORK HISTORY</div>
-            <div className="role left">
-              FULLSTACK DEVELOPER - ANI CONSULTING • 09/2019 - CURRENT
+                        <a
+                            className="left"
+                            target="_blank"
+                            href="http://alexcphillips.com"
+                        >
+                            http://alexcphillips.com
+                        </a>
+                        <div style={{ marginBottom: "1rem" }} />
+                    </div>
+                    <div className="section">
+                        <div className="section-head left">WORK HISTORY</div>
+                        <div className="role left">
+                            NODEJS DEVELOPER - ANI CONSULTING • 09/2019 -
+                            CURRENT
+                        </div>
+                        <ul className="left">
+                            <li>Built REST APIs with Express server</li>
+                            <li>
+                                Converted Excel file data to JSON for MongoDB
+                                storage
+                            </li>
+                            <li>Used Redis pub sub for microservices</li>
+                            <li>Containerized applications with Docker</li>
+                            <li>
+                                Created unit tests with Jest for 100% coverage
+                            </li>
+                            <li>
+                                Deploy Docker containers to ec2s on AWS with
+                                docker-compose
+                            </li>
+                            <li>Managed security groups for ec2 instances</li>
+                            <li>
+                                Created scripts to automatically open and shut
+                                down ec2s as needed for development
+                            </li>
+                            <li>
+                                Built financial reporting and Excel data
+                                processing applications including frontend React
+                                components
+                            </li>
+                        </ul>
+                        <div className="footer">
+                            <a
+                                className="download-link"
+                                href=""
+                                onClick={saveResume}
+                            >
+                                Download Resume
+                            </a>
+                            <div className="small-spacer" />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <ul className="left">
-              <li>Built REST APIs with Express server</li>
-              <li>Converted Excel file data to JSON for MongoDB storage</li>
-              <li>Used Redis pub sub for microservices</li>
-              <li>Containerized applications with Docker</li>
-              <li>Created unit tests with Jest for 100% coverage</li>
-              <li>
-                Deploy Docker containers to ec2s on AWS with docker-compose
-              </li>
-              <li>Managed security groups for ec2 instances</li>
-              <li>
-                Created scripts to automatically open and shut down ec2s as
-                needed for development
-              </li>
-              <li>
-                Built financial reporting and Excel data processing applications
-                including frontend React components
-              </li>
-            </ul>
-            <div className="footer">
-              <a className="download-link" href="" onClick={saveResume}>
-                Download Word Document
-              </a>
-              <div className="small-spacer" />
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
