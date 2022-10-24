b181321695a58707b8319330ee23d9bb06b734fa
Alexander Phillips <alexphillipsdev@gmail.com>
2022-10-24 09:26:48 -0500

    remove unused code

--- server/scripts/dbSeed.js
+++ server/scripts/dbSeed.js
@@ -1,17 +1,1 @@
-const { find, insertMany } = require("../mongo/operations");
-const docs = require("./familyImages.json");
-
-exports.seedFamilyImages = async () => {
-  const dbDocs = await find("images", { page: "family" }, { name: 1, _id: 0 });
-
-  const dbDocNames = dbDocs.map((doc) => doc.name);
-  const missingDocs = [];
-
-  for (const doc of docs) {
-    if (!dbDocNames.includes(doc.name)) {
-      missingDocs.push(doc);
-    }
-  }
-
-  if (missingDocs.length) await insertMany("images", missingDocs);
-};
+// const { find, insertMany } = require("../mongo/operations");
